97242febe79475217378d23d5c50dc88a1277ee6
Juan De Jesus <Juan De Jesus>
2017-06-22 22:14:43 -0400

    index.js fixes for deployment.

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/juan1003.github.io/docs/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
