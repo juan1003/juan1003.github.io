87506163eee64a83422fcd50f83a3bcad025b891
Juan De Jesus <jdejesus93@gmail.com>
2019-06-27 09:49:45 -0400

    Separating lists initializers.

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
 (function(){
     'use strict'
 
-    listInit();
-})();
+    jobLististInit()
+    repositoryListInit()
+})()
 
 async function getRepos() {
     const response = await fetch('https://api.github.com/users/juan1003/repos');
@@ -10,8 +11,19 @@ async function getRepos() {
     return data;
 }
 
-async function listInit() {
-    const repos = await getRepos();
+async function repositoryListInit() {
+    const repositories = await getRepos();
+
+    repos.map(repo => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<h4><a id="repo-link" href="${repo.html_url}" target="blank">${repo.name}</a></h4>
+                              <p>${repo.description}</p>`
+
+        document.querySelector('#repo-list').appendChild(listItem)
+    })
+}
+
+function jobLististInit() {
     const experience = [
         {title: "Software Developer", company: "Qinetix Group LLC.", from: "April 2018", until: "Present", location: "Guaynabo, PR"},
         {title: "Software Engineer", company: "Agordia", from: "April 2017", until: "February 2018", location: "Guaynabo, PR"},
@@ -21,16 +33,8 @@ async function listInit() {
     experience.map(job => {
         const listItem = document.createElement('li');
         listItem.innerHTML = `<h4>${job.title}</h4>
-                              <p>${job.company} | ${job.from} - ${job.until} | ${job.location}</p>`;
+                              <p>${job.company} | ${job.from} - ${job.until} | ${job.location}</p>`
         
-        document.querySelector('#job-list').appendChild(listItem);
-    })
-    
-    repos.map(repo => {
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `<h4><a id="repo-link" href="${repo.html_url}" target="blank">${repo.name}</a></h4>
-                              <p>${repo.description}</p>`;
-
-        document.querySelector('#repo-list').appendChild(listItem);
+        document.querySelector('#job-list').appendChild(listItem)
     })
 }
