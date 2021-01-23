bf31c605a8728161eeacc60a27e35cb6e4f85746
Juan De Jesus <jdejesus93@gmail.com>
2021-01-22 20:55:26 -0400

    Fixing some bug

--- js/app.js
+++ js/app.js
@@ -3,8 +3,9 @@ fetch("https://api.github.com/users/juan1003/repos")
     .then(data => {
         data.map(repository => {
             const div = document.createElement('div');
+            div.className = 'repository';
             div.innerHTML = `
-                <a class="repository" href="${repository.html_url}" target="_blank">${repository.name}</a>
+                <a href="${repository.html_url}" target="_blank">${repository.name}</a>
                 <p>${repository.description}</p>
             `;
             document.querySelector("#repo-list").appendChild(li);
